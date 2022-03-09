e6c1f98f253e4df9578846c19b044c6ed56b03c4
Dhruvi Patel <DhruviPatel@dhruvis-mbp.quinnipiac.edu>
2022-03-09 18:12:00 -0500

    fish ASCII added

--- src/animalPicker.java
+++ src/animalPicker.java
@@ -10,7 +10,7 @@ public class animalPicker {
 
 		Scanner question = new Scanner(System.in);
 
-		System.out.println("Would you like to see a dog or cat?");
+		System.out.println("Which animal would you like to see: \n>dog \n>cat \n>fish");
 
 		String answer = question.nextLine();
 
@@ -21,6 +21,11 @@ public class animalPicker {
 		} else if (answer.equalsIgnoreCase("dog")) {
 
 			System.out.println("     |\\_/|                  \n     | @ @   Woof! \n     |   <>              _  \n     |  _/\\------____ ((| |))\n     |               `--\' |   \n ____|_       ___|   |___.\' \n/_/_____/____/_______|");
+			
+		} else if (answer.equalsIgnoreCase("fish")) {
+			
+			System.out.println("           FISHKISSFISHKIS               \n       SFISHKISSFISHKISSFISH            F\n    ISHK   ISSFISHKISSFISHKISS         FI\n  SHKISS   FISHKISSFISHKISSFISS       FIS\nHKISSFISHKISSFISHKISSFISHKISSFISH    KISS\n  FISHKISSFISHKISSFISHKISSFISHKISS  FISHK\n      SSFISHKISSFISHKISSFISHKISSFISHKISSF\n  ISHKISSFISHKISSFISHKISSFISHKISSF  ISHKI\nSSFISHKISSFISHKISSFISHKISSFISHKIS    SFIS\n  HKISSFISHKISSFISHKISSFISHKISS       FIS\n    HKISSFISHKISSFISHKISSFISHK         IS\n       SFISHKISSFISHKISSFISH            K\n         ISSFISHKISSFISHK               ");
+			
 		} else {
 
 			System.out.println("That is not an option");
@@ -43,6 +48,12 @@ public class animalPicker {
 					
 					break;
 
+				} else if (answer2.equalsIgnoreCase("fish")) {
+					
+					System.out.println("           FISHKISSFISHKIS               \n       SFISHKISSFISHKISSFISH            F\n    ISHK   ISSFISHKISSFISHKISS         FI\n  SHKISS   FISHKISSFISHKISSFISS       FIS\nHKISSFISHKISSFISHKISSFISHKISSFISH    KISS\n  FISHKISSFISHKISSFISHKISSFISHKISS  FISHK\n      SSFISHKISSFISHKISSFISHKISSFISHKISSF\n  ISHKISSFISHKISSFISHKISSFISHKISSF  ISHKI\nSSFISHKISSFISHKISSFISHKISSFISHKIS    SFIS\n  HKISSFISHKISSFISHKISSFISHKISS       FIS\n    HKISSFISHKISSFISHKISSFISHK         IS\n       SFISHKISSFISHKISSFISH            K\n         ISSFISHKISSFISHK               ");
+					
+					break;
+					
 				} else {
 
 					System.out.println("That is not an option");
